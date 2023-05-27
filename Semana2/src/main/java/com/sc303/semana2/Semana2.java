bfc26e4acac7cab6946668c6e2054ecb79f4c1f6
IsaacRuizS <isaacruizse@gmail.com>
2023-05-27 11:00:54 -0600

    Semana 2

--- Semana2/src/main/java/com/sc303/semana2/Semana2.java
+++ Semana2/src/main/java/com/sc303/semana2/Semana2.java
@@ -1,17 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.sc303.semana2;
 
-/**
- *
- * @author Laboratorios
- */
+import javax.swing.JOptionPane;
+
 public class Semana2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //ejercicio1();
+        presentacion2();
+    }
+    
+    public static void presentacion2(){
+        int edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese  la edad"));
+        if(edad>=18){
+            JOptionPane.showMessageDialog(null, "Usted puede votar");
+        }else{
+            JOptionPane.showMessageDialog(null, "Usted puede NO votar");
+        }
+
+        
+    }
+    
+    public static void ejercicio1(){
+        //sume y promedie 4 nuemros 
+        int sumaTotal = 0;
+        double promedioTotal = 0;
+        /*int n1 = 25;
+        int n2 = 10;
+        int n3 = 40;
+        int n4 = 12;
+        
+        sumaTotal = n1+n2+n3+n4;
+        promedioTotal = sumaTotal/4.0;*/
+        for (int index = 0; index < 4; index++) {
+            int showIndex = index+1;
+            int numeroASumar = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero a suma en la posicion "+showIndex));
+            sumaTotal = sumaTotal + numeroASumar;
+            promedioTotal = sumaTotal/4.0;
+            
+        }
+        JOptionPane.showMessageDialog(null, "Suma = " + sumaTotal+ "\n El promedio total = "+promedioTotal);
     }
 }
